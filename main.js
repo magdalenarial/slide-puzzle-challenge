2408bab81363d24dca621a0e24969d9f606b0f99
Magdalena Rial <57687897+magdalenarial@users.noreply.github.com>
2021-01-23 00:55:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -48,7 +48,10 @@
     document.getElementById('row-4-col-3').textContent = imagePosition[14];
     document.getElementById('row-4-col-3').style.background = 'url('+imagePosition[14]+'.jpg)'; 
 
- 
+    document.getElementById('row-4-col-4').textContent = 16;
+    document.getElementById('row-4-col-4').style.background = 'none'; 
+
+    id_blank_space = "row-4-col-4";
 }
 
 boardPositions = {
@@ -75,15 +78,17 @@ var id_blank_space = "row-4-col-4";
 //Mueve las fichas de un lado a otro
 function shiftPuzzlepieces(el) {   
    if(is_blank_sibling(el.id, id_blank_space)){
-       console.log(document.getElementById(el.id));
-       console.log(document.getElementById(id_blank_space));
-        let hold_image = document.getElementById(el.id).style.cssText;
-        document.getElementById(el.id).style.cssText = document.getElementById(id_blank_space).style.cssText;
-        document.getElementById(id_blank_space).style.cssText = hold_image;
+        let hold_image = document.getElementById(el.id).style.background;
+        document.getElementById(el.id).style.background = 'none';
+        document.getElementById(el.id).style.background = document.getElementById(id_blank_space).style.background;
+        document.getElementById(id_blank_space).style.background = hold_image;
         let hold_textContext= document.getElementById(el.id).textContent;
         document.getElementById(el.id).textContent = document.getElementById(id_blank_space).textContent;
         document.getElementById(id_blank_space).textContent = hold_textContext;
         id_blank_space = el.id;
+   }    
+   if(check_game_over()){
+       alert('Ganaste la partida');
    }
 }
 
@@ -91,3 +96,29 @@ function is_blank_sibling(el_id, blank){
     return boardPositions[el_id]["siblings"].includes(blank);
 }
 
+
+//Que las imagenes coincidan con el textContent para volver a su lugar inicial y eso significar que ganaste
+function check_game_over(){
+    var cell1 = document.getElementById('row-1-col-1').textContent;
+    var cell2 = document.getElementById('row-1-col-2').textContent;
+    var cell3 = document.getElementById('row-1-col-3').textContent;
+    var cell4 = document.getElementById('row-1-col-4').textContent;
+    var cell5 = document.getElementById('row-2-col-1').textContent;
+    var cell6 = document.getElementById('row-2-col-2').textContent;
+    var cell7 = document.getElementById('row-2-col-3').textContent;
+    var cell8 = document.getElementById('row-2-col-4').textContent;
+    var cell9 = document.getElementById('row-3-col-1').textContent;
+    var cell10 = document.getElementById('row-3-col-2').textContent;
+    var cell11 = document.getElementById('row-3-col-3').textContent;
+    var cell12 = document.getElementById('row-3-col-4').textContent;
+    var cell13 = document.getElementById('row-4-col-1').textContent;
+    var cell14 = document.getElementById('row-4-col-2').textContent;
+    var cell15 = document.getElementById('row-4-col-3').textContent;
+    var cell16 = document.getElementById('row-4-col-4').textContent;
+  
+    if((cell1 == '1') && (cell2 == '2') && (cell3 == '3') && (cell4 == '4') && (cell5 == '5') && (cell6 == '6') && (cell7 == '7') && (cell8 == '8') && (cell9 == '9') && (cell10 == '10') && (cell11 == '11') && (cell12 == '12') && (cell13 == '13') && (cell14 == '14') && (cell15 == '15') && (cell16 == '16')){
+        return true
+    } else {
+        return false 
+    }
+}
